aa901df671de43ea02c7c283aa60ef1b5a0fb635
mon1tor <69232005+mon1t0r@users.noreply.github.com>
2022-04-25 17:59:17 +0300

    Added crafts and materials, new localization

--- src/main/java/com/mon1tor/radiocraft/block/ModBlocks.java
+++ src/main/java/com/mon1tor/radiocraft/block/ModBlocks.java
@@ -4,12 +4,10 @@ import com.mon1tor.radiocraft.Radiocraft;
 import com.mon1tor.radiocraft.block.custom.RadioChargerBlock;
 import com.mon1tor.radiocraft.item.ModItemGroup;
 import com.mon1tor.radiocraft.item.ModItems;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -28,9 +26,12 @@ public class ModBlocks {
         registerBlockItem(name, result);
         return result;
     }
-
     private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(ModItemGroup.RADIO_GROUP)));
+        registerBlockItem(name, block, ModItemGroup.RADIO_GROUP);
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block, ItemGroup itemGroup) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(itemGroup)));
     }
 
     public static void register(IEventBus eventBus) {
--- src/main/java/com/mon1tor/radiocraft/item/ModItems.java
+++ src/main/java/com/mon1tor/radiocraft/item/ModItems.java
@@ -1,7 +1,7 @@
 package com.mon1tor.radiocraft.item;
 
 import com.mon1tor.radiocraft.Radiocraft;
-import com.mon1tor.radiocraft.item.custom.*;
+import com.mon1tor.radiocraft.item.custom.RadioItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
@@ -11,8 +11,12 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Radiocraft.MOD_ID);
 
-    public static final RegistryObject<Item> RADIO = ITEMS.register("radio",
-            () -> new RadioItem());
+    public static final RegistryObject<Item> RADIO = ITEMS.register("radio", () -> new RadioItem());
+    public static final RegistryObject<Item> ANTENNA = ITEMS.register("antenna", () -> new Item(new Item.Properties().tab(ModItemGroup.RADIO_GROUP)));
+    public static final RegistryObject<Item> BATTERY = ITEMS.register("battery", () -> new Item(new Item.Properties().tab(ModItemGroup.RADIO_GROUP)));
+    public static final RegistryObject<Item> DISPLAY = ITEMS.register("display", () -> new Item(new Item.Properties().tab(ModItemGroup.RADIO_GROUP)));
+    public static final RegistryObject<Item> CHIP_1 = ITEMS.register("chip_1", () -> new Item(new Item.Properties().tab(ModItemGroup.RADIO_GROUP)));
+    public static final RegistryObject<Item> CHIP_2 = ITEMS.register("chip_2", () -> new Item(new Item.Properties().tab(ModItemGroup.RADIO_GROUP)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
